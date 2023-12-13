17bf6772de4c18b5280837e28d7c8eb5c0e22945
do5do <do151015@gmail.com>
2023-12-13 14:11:07 +0900

    init

---
+++ src/main/java/com/zerobase/funding/FundingApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.funding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundingApplication.class, args);
+	}
+
+}
