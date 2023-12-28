0186d10bb7c302b0330618f71e2dca06ed3e1886
do5do <do151015@gmail.com>
2023-12-28 17:47:45 +0900

    refactor: 회원 조회, 수정 시 권한 체크 추가

--- src/main/java/com/zerobase/funding/api/member/service/MemberService.java
+++ src/main/java/com/zerobase/funding/api/member/service/MemberService.java
@@ -2,6 +2,7 @@ package com.zerobase.funding.api.member.service;
 
 import static com.zerobase.funding.api.exception.ErrorCode.MEMBER_NOT_FOUND;
 
+import com.zerobase.funding.api.auth.service.AuthenticationService;
 import com.zerobase.funding.api.member.dto.model.MemberDto;
 import com.zerobase.funding.api.member.dto.MemberEditRequest;
 import com.zerobase.funding.api.member.exception.MemberException;
@@ -16,14 +17,18 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class MemberService {
     private final MemberRepository memberRepository;
+    private final AuthenticationService authenticationService;
 
     public MemberDto memberInfo(String memberKey) {
-        return MemberDto.fromEntity(findByMemberKeyOrThrow(memberKey));
+        Member member = findByMemberKeyOrThrow(memberKey);
+        authenticationService.checkAccess(memberKey, member);
+        return MemberDto.fromEntity(member);
     }
 
     @Transactional
     public MemberDto memberEdit(MemberEditRequest request, String memberKey) {
         Member member = findByMemberKeyOrThrow(memberKey);
+        authenticationService.checkAccess(memberKey, member);
         member.updateMember(request);
         return MemberDto.fromEntity(member);
     }
