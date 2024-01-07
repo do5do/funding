610394a9f1b4b527748c52863dc40cbcb3368eb4
do5do <do151015@gmail.com>
2024-01-08 01:14:12 +0900

    feat: 참여한 펀딩 조회 api 작성 - 한달 단위로 조회 가능

--- src/main/java/com/zerobase/funding/domain/funding/repository/CustomFundingRepository.java
+++ src/main/java/com/zerobase/funding/domain/funding/repository/CustomFundingRepository.java
@@ -2,10 +2,15 @@ package com.zerobase.funding.domain.funding.repository;
 
 import com.zerobase.funding.domain.funding.entity.Funding;
 import com.zerobase.funding.domain.funding.entity.Status;
+import com.zerobase.funding.domain.member.entity.Member;
 import com.zerobase.funding.domain.reward.entity.Reward;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface CustomFundingRepository {
 
     List<Funding> findAllByRewardInAndStatusFetch(List<Reward> rewards, Status status);
+
+    List<Funding> findAllByMemberPerMonthFetch(Member member, LocalDateTime startDateTime,
+            LocalDateTime endDateTime);
 }
--- src/main/java/com/zerobase/funding/domain/funding/repository/impl/CustomFundingRepositoryImpl.java
+++ src/main/java/com/zerobase/funding/domain/funding/repository/impl/CustomFundingRepositoryImpl.java
@@ -3,13 +3,16 @@ package com.zerobase.funding.domain.funding.repository.impl;
 import static com.zerobase.funding.domain.delivery.entity.QDelivery.delivery;
 import static com.zerobase.funding.domain.funding.entity.QFunding.funding;
 import static com.zerobase.funding.domain.member.entity.QMember.member;
+import static com.zerobase.funding.domain.reward.entity.QReward.reward;
 
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.zerobase.funding.domain.funding.entity.Funding;
 import com.zerobase.funding.domain.funding.entity.Status;
 import com.zerobase.funding.domain.funding.repository.CustomFundingRepository;
+import com.zerobase.funding.domain.member.entity.Member;
 import com.zerobase.funding.domain.reward.entity.Reward;
+import java.time.LocalDateTime;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 
@@ -24,6 +27,16 @@ public class CustomFundingRepositoryImpl implements CustomFundingRepository {
                 .join(funding.member, member).fetchJoin()
                 .join(funding.delivery, delivery).fetchJoin()
                 .where(funding.reward.in(rewards), statusEq(status))
+                .fetch();
+    }
+
+    @Override
+    public List<Funding> findAllByMemberPerMonthFetch(Member member,
+            LocalDateTime startDateTime, LocalDateTime endDateTime) {
+        return queryFactory.selectFrom(funding)
+                .join(funding.reward, reward).fetchJoin()
+                .where(funding.member.eq(member),
+                        funding.createdDate.between(startDateTime, endDateTime))
                 .fetch();
     }
 
