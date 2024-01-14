7db97c714f41e60b716a6e3d2d826af6a1fe8608
do5do <do151015@gmail.com>
2024-01-14 17:37:30 +0900

    style: profile 수정

--- src/main/java/com/zerobase/funding/api/config/LocalRedisConfig.java
+++ src/main/java/com/zerobase/funding/api/config/LocalRedisConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import redis.embedded.RedisServer;
 
-@Profile({"local"})
+@Profile("local")
 @Configuration
 public class LocalRedisConfig {
 
