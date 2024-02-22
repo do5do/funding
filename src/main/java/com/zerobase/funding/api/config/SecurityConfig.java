4da921b36ce959d10e90ba3d5875df2437c1ab9e
dohee Kim <do151015@gmail.com>
2024-02-22 17:15:47 +0900

    OAuth2 handler 추가 (#11)
    * refactor: security관련 수정
    - security 적용하지 않을 리소스 분리
    - PrincipalDetails getName() 메서드 수정 (DefaultOAuth2User 참고)
    
    * doc: troubleshooting 관련 블로그 url 추가
    
    * refactor: 단순 수정
    
    * refactor: oauth2 failure handler 추가
    
    * doc: readme 수정

--- src/main/java/com/zerobase/funding/api/config/SecurityConfig.java
+++ src/main/java/com/zerobase/funding/api/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.zerobase.funding.api.config;
 
 import com.zerobase.funding.api.auth.handler.CustomAccessDeniedHandler;
 import com.zerobase.funding.api.auth.handler.CustomAuthenticationEntryPoint;
+import com.zerobase.funding.api.auth.handler.OAuth2FailureHandler;
 import com.zerobase.funding.api.auth.handler.OAuth2SuccessHandler;
 import com.zerobase.funding.api.auth.jwt.TokenAuthenticationFilter;
 import com.zerobase.funding.api.auth.jwt.TokenExceptionFilter;
@@ -33,8 +34,7 @@ public class SecurityConfig {
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return web -> web.ignoring()
-                .requestMatchers("/error")
-                .requestMatchers("/favicon.ico");
+                .requestMatchers("/error", "/favicon.ico");
     }
 
     @Bean
@@ -63,6 +63,7 @@ public class SecurityConfig {
                 .oauth2Login(oauth ->
                         oauth.userInfoEndpoint(c -> c.userService(oAuth2UserService))
                                 .successHandler(oAuth2SuccessHandler)
+                                .failureHandler(new OAuth2FailureHandler())
                 )
 
                 .addFilterBefore(tokenAuthenticationFilter,
